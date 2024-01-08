e60f445f90dae89273da91b2845413957258f357
lisa56530 <145032722+lisa56530@users.noreply.github.com>
2024-01-08 15:36:18 +0100

    Update CountryRepository.java

--- src/main/java/monprojet/dao/CountryRepository.java
+++ src/main/java/monprojet/dao/CountryRepository.java
@@ -7,20 +7,18 @@ import org.springframework.data.jpa.repository.Query;
 
 import monprojet.entity.City;
 import monprojet.entity.Country;
-import org.springframework.data.rest.core.config.Projection;
 
 // This will be AUTO IMPLEMENTED by Spring 
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
-    @Query ("SELECT SUM (population) AS population "
-    + "FROM city "
-    + "WHERE country.id =: id ")
+    @Query ("SELECT SUM(population) AS population "
+    + "FROM City c "
+    + "WHERE c.Country.id = :id ")
     public int sommePop (int id) ;
 
-    @Query ("Select nom.pays as Nom , sum (population) as population "
-            + "FROM country "
-            + "WHERE id.country = id.city " )
-            public List<Projection> lesPays () ;
+    @Query ("Select c.country.nom as Nom , sum (c.population) as population "
+            + "FROM City c" )
+    public List<Projection> lesPays () ;
 
 }
 
